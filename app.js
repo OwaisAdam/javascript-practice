d3899aaa10e23757c269edbfc86e36745242adf7
owaisadam <owaisadam500@gmail.com>
2024-02-23 15:43:19 +0500

    basic practice of javascript

---
+++ app.js
@@ -0,0 +1,146 @@
+// chapter no (1) start
+// QNO 1
+alert("HelloWorld")
+// QNO 2
+alert("Enter! please enter your valid password")
+// QNO 3
+alert("Welcome to js land..\n Happy Coding")
+// QNO 4
+alert("Welcome to js land")
+alert("Happy Coding\nPervent this page from creating additional dialogs")
+// QNO 5
+document.write("Hello..I can run JS through my web brower's console")
+// QNO 6
+// QNO 7 
+// done
+// chapter no (1) End
+
+// chapter no (2) start
+// QNO 1
+var username
+// QNO 2
+var myname = "OwaisAdam"
+// QNO 3
+var messege = "Hello World"
+
+alert(messege)
+
+// QNO 4
+var Fullname = "johndoe"
+var Age = "15"
+var certificate  =  "Certified Mobile Application Development"
+
+alert(name)
+alert(Age)
+alert(certificate)
+// QNO 5                                     
+ var pizza = "pizza\npizz\npiz\npi\np"
+
+ alert(pizza)
+
+
+// QNO 6
+
+
+
+ var email = "owaisadam@gmail.com"
+ alert(email)
+// QNO 7
+
+var book = "A smater way to learn javascript"
+
+alert( " i am trying to learn javascript " + book)
+
+// QNO 8
+
+document.write("YAh! I can write HTML  content through Javascript")
+
+// QNO 9
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// chapter no (2) END
+
+// chapter no (3) start
+// QNO 1
+var age = 20
+alert(age)
+// QNO 2
+
+//
+
+// QNO 3
+var birthYear = "My birthYear is 1999"
+document.write("birthYear" + " \nData type of my ddeclared variable is number")
+
+// QNO 4
+
+var xyz = "wwwrexcentercom"
+var visitorsname = "adam"
+var producttitle  = "kurtashalwar"
+var qty = 15
+
+document.write("xyz"+"visitorname"+"producttitle"+"qty")
+
+// chapter no (3) End
+
+
+// chapter no (1) start
+
+// section (Alerts)
+// Qno 1
+alert("umair")
+alert("haji")
+alert("owaisdam500@gmail.com")
+alert("03003771266")
+ 
+// Qno 2
+alert("you are learning JavaScript!")
+alert("Hello students")
+
+// chapter no (1) end
+
+// chapter no (2) start
+// section (Variables for string)
+//      Qno 1
+var camelcase
+// Qno 2
+var c = "cricket"
+var js = "javascript"
+//  Qno 3
+var builders = "hibagroup of compaines"
+alert(builders)
+//  Qno 4
+var bestMan = "Charlie"
+var bestMan =  "Johndoe"
+// chapter no (2) end
+
+// chapter no (3) start
+// section (Variables for numbers)
+//      Qno 1
+
+var CaseQty
+//      Qno 2
+
+var casrQty = 144
+
+//      Qno 2
+var num = 9
+//      Qno 3   
+
+var sum
+sum = 12 + 12
+
+//      Qno 4
+
+var mercTotal = 100
+var shippingCharge  = 100
+var orderTotal = mercTotal + shippingCharge
+console.log(orderTotal)
+
+//      Qno 5
+
+var number = 15
+var number = number + 5
+console.log("number")
+
+// chapter no (3) end
